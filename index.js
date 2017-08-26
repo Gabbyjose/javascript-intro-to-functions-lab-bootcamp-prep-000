818047e4f7ae2f58c36816e95f7bfa4c35ba0b64
Gabriela Josebachvili <gabbyjose@gmail.com>
2017-08-26 16:27:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 function shout (string){
   return string.toUpperCase();
 }
+
+function whisper(string){
+  return string.toLowerCase();
+}
+
+function logShout(string){
+  console.log(shout(string));
+}
+
+function logWhisper(string){
+  console.log(whisper(string));
+}
+
+function sayHiToGrandma(string){
+  if (string == "I love you, Grandma."){
+    return string;
+  }
+  else if (string == uppercase){
+    return "YES INDEED!";
+  }
+  else if (string == lowercase){
+    return "I can't hear you!"l
+  }
+}
